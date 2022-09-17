36162d43d728090e38f9f3ca56b13983a4b09949
luizgustavodiniz <gustavo.diniz.gon@gmail.com>
2022-09-17 11:35:45 -0300

    finalização do projeto

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ const completeContainer = document.querySelector('#complete-container')
 const taskCompletedContainer = document.querySelector('#tasks-completed')
 const messageAddTaskContainer = document.querySelector('#message-add')
 const messageCheckTaskContainer = document.querySelector('#message-complete')
+const editContainer = document.querySelector('#edit')
+const formEditTask = document.querySelector('#form-edit-task')
 const body = document.querySelector('body')
 
 const generateId = () => String(Math.round(Math.random() * 1000))
@@ -52,8 +54,30 @@ const refreshCompleteContainer = () => {
 
 }
 
-const editTask = () => {
-  alert('Editou!')
+
+
+const closeEditContainer = (event, id) => {
+  const element = event.target
+
+  if(element.id === 'close'){
+    element.parentElement.parentElement.parentElement.classList.remove('active')
+   }
+
+  if(element.tagName !== "FORM" || element.tagName !== "H2" || element.tagName !== "INPUT" || element.tagName !== "BUTTON"){
+    element.classList.remove('active')
+  }
+  
+}
+
+const editContainerShow = (event) => {
+  editContainer.classList.add('active')
+
+  const id = event.target.dataset.id
+
+  editContainer.addEventListener('click', closeEditContainer(event, id))
+
+  formEditTask.setAttribute('data-id', id)
+
 }
 
 const createTask = (task, index) => {
@@ -63,7 +87,7 @@ const createTask = (task, index) => {
     <span>${task.name}</span>
       <div class="actions">
         <button><i onclick="checkTask(event)" data-id="${task.id}" class="fas fa-check"></i></button>
-        <button><i onclick="editTask(event)" data-id="${task.id}"  class="fas fa-edit"></i></button>
+        <button><i onclick="editContainerShow(event)" data-id="${task.id}"  class="fas fa-edit"></i></button>
         <button><i data-id="${task.id}"  class="fas fa-trash-alt"></i></button>
       </div>
     </label>
@@ -87,21 +111,33 @@ const checkTask = event => {
   let currentTask = event.target
   let currentTaskIndex = currentTask.dataset.id
 
-  bankTasks.filter(item => item.id === currentTaskIndex)
+  const item = bankTasks.filter(item => item.id === currentTaskIndex)
    .reduce((acc, item) => {
       item.status = true
-     completedTasks.push(item)
+     
+      return item
    }, {})
+
+   completedTasks.push(item)
+
+   console.log(completedTasks)
 
    localStorage.setItem('completedTasks', JSON.stringify(completedTasks))
 
-   let tasks = bankTasks.filter(item => item.status !== true)
+   let tasks = bankTasks
+   .filter(item => item.id !== currentTaskIndex)
 
-   localStorage.setItem('tasks', JSON.stringify(tasks))
+   bankTasks = tasks
+    
+
+   console.log(tasks)
+   console.log(bankTasks)
+
+   localStorage.setItem('tasks', JSON.stringify(bankTasks))
 
    refreshCompleteContainer()
 
-   refresh(tasks)
+   refresh(bankTasks)
 
    //refreshTasksArray(currentTaskIndex)
 }
@@ -129,6 +165,8 @@ const getInputValue = event => {
    }
 
    insertTaskIntoBank(inputTaskValue)
+
+   event.target.task.value = ''
 }
 
 
@@ -157,13 +195,22 @@ const closeModals = event => {
 }
 
 
-
-
 const getSearch = event => {
   event.preventDefault()
   const searchValue = event.target.value
-    
-  let tasksFilter = bankTasks.filter(task => task.name.includes(searchValue))
+
+  let teste = bankTasks
+   .filter(task => task.name.includes(searchValue) && task.status !== true)
+   .reduce((acc, item) => {
+    return item
+   }, {}) 
+
+   console.log(teste)
+
+
+  let tasksFilter = bankTasks
+   .filter(task => task.name.includes(searchValue)  && task.status !== true)
+                            
 
   if(searchValue === ''){
     refresh(bankTasks)
@@ -174,12 +221,47 @@ const getSearch = event => {
   localStorage.setItem('filteredTasks', JSON.stringify(filteredTasks))
   refresh(filteredTasks)
 
+
 }
+
+
+const editTask = (event) => {
+  event.preventDefault()
+  const inputEditValue = event.target.task.value
+  const id = event.target.dataset.id
+ 
+  let task = bankTasks
+              .filter(task => task.id === id)
+              .reduce((acc, item) => {
+
+                return item
+
+              }, {})
+
+ 
+
+ bankTasks.forEach(task => {
+
+  if(task.id === id){
+    task.name = inputEditValue
+  }
+
+ })
+
+  localStorage.setItem('tasks', JSON.stringify(bankTasks))
+  refresh(bankTasks)
+  
+  editContainer.classList.remove('active')
+
+ }
+
 
 formTaskValue.addEventListener('submit', getInputValue)
 formSearchValue.addEventListener('input', getSearch)
 completeTasks.addEventListener('click', getCompleteTasks)
 completeContainer.addEventListener('click', closeCompleteTasksContainer)
+editContainer.addEventListener('click', closeEditContainer)
+formEditTask.addEventListener('submit', editTask)
 //body.addEventListener('click', closeModals)
 
 
